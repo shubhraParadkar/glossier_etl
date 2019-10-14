0294e89ff3501400a53b04181db97c3af14515f7
Shubhra Paradkar <shubhra.paradkar@gmail.com>
2019-10-14 19:57:09 -0400

    Add files via upload
    ADDING initial files t

---
+++ etl.py
@@ -0,0 +1,106 @@
+import os
+import glob
+import psycopg2
+import pandas as pd
+import numpy as np
+from sql_queries import *
+
+
+# cur parameter: actual connection to DATABASE
+# filepath parameter: JSON file path
+# this function receive a connection and json path parameters.
+# read the information using pandas and insert into database.
+def process_song_file(cur, filepath):
+    # open song file
+    df = pd.read_json(filepath, orient='index')
+    print(df)
+    # insert song record
+    user_data = df[['user_id', 'contact_email', 'phone', 'browser_ip','order_number']].columns[1].tolist()
+    cur.execute(user_table_insert, user_data)
+
+    # insert artist record
+    orders_data = df[['id', 'total_price', 'name',
+                      'order_number', 'order_status_url']].values[0].tolist()
+    cur.execute(orders_table_insert, orders_data)
+
+# this function receive a connection and json path parameters.
+# read the information using pandas and insert into database.
+
+
+# def process_log_file(cur, filepath):
+#     # open log file
+#     df = pd.read_json(filepath, lines=True)
+#
+#     # filter by NextSong action
+#     df = df[df['page'] == 'NextUser']
+#
+#     # convert timestamp column to datetime
+#     t = pd.to_datetime(df['ts'], unit='ms')
+#
+#     # insert time data records
+#     time_data = np.transpose(np.array([df['ts'].values, t.dt.hour.values, t.dt.day.values, t.dt.week.values,
+#                                        t.dt.month.values, t.dt.year.values, t.dt.weekday.values]))
+#     column_labels = ('ts', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+#     time_df = pd.DataFrame(data=time_data, columns=column_labels)
+#
+#     # iterate into every row and create a insert statement for every row
+#     for i, row in time_df.iterrows():
+#         cur.execute(time_table_insert, list(row))
+#
+#     # load user table
+#     user_df = df[['userId', 'contact_email', 'phone', 'browser_ip', 'order_number']]
+#
+#     # insert user records
+#     for i, row in user_df.drop_duplicates().iterrows():
+#         cur.execute(user_table_insert, row)
+#
+#     # insert songplay records
+#     for index, row in df.iterrows():
+#         # get songid and artistid from song and artist tables
+#         cur.execute(orders_select, (row.order.encode('utf-8'),
+#                                   row.user.encode('utf-8'), row.length))
+#
+#         # retrieve the information by tuples
+#         results = cur.fetchone()
+#         # asing song_id and artist_id into variables
+#         id, user_id = results if results else (None, None)
+#
+#         # insert songplay record
+#         songplay_data = (row.ts, row.user_id, row.contact_email, id, order_number,
+#                          row.sessionId, row.location, row.userAgent)
+#         cur.execute(orders_table_insert, orders_data)
+
+# iterate into every directory and retrieve json files to process data
+
+
+def process_data(cur, conn, filepath, func):
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
+
+def main():
+    conn = psycopg2.connect("host=data-engineering-test.dev.glossier.io dbname=shubhra_db user=shubhra password=iheartcloudpaint port=80")
+    cur = conn.cursor()
+
+    process_data(cur, conn, filepath='/Users/shubhraparadkar/Documents/data', func=process_song_file)
+    #process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sql_queries.py
@@ -0,0 +1,79 @@
+# DROP TABLES
+
+#user_table_drop = "DROP TABLE IF EXISTS songplays"
+users_table_drop = "DROP TABLE IF EXISTS users"
+orders_table_drop = "DROP TABLE IF EXISTS orders"
+#artist_table_drop = "DROP TABLE IF EXISTS artists"
+#time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+#songplays_table_create = ("""CREATE TABLE IF NOT EXISTS songplays \
+#(songplay_id SERIAL PRIMARY KEY, \
+#start_time bigint NOT NULL, user_id int NOT NULL, level varchar, song_id varchar, \
+#artist_id varchar, session_id int, location varchar, user_agent varchar);
+#"")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users \
+(user_id int PRIMARY KEY, contact_email varchar NOT NULL, phone int NOT NULL, \
+browser_ip varchar, order_number int NOT NULL);
+""")
+
+orders_table_create = ("""CREATE TABLE IF NOT EXISTS orders \
+(id varchar PRIMARY KEY, total_price numeric NOT NULL, name varchar NOT NULL, \
+order_number int NOT NULL, order_status_url varchar NOT NULL);
+""")
+
+# artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists \
+# (artist_id varchar PRIMARY KEY, name varchar NOT NULL, location varchar, \
+# lattitude numeric, longitude numeric);
+# """)
+#
+# time_table_create = ("""CREATE TABLE IF NOT EXISTS time \
+# (start_time bigint PRIMARY KEY, hour int NOT NULL, day int NOT NULL, \
+# week int NOT NULL, month int NOT NULL, year int NOT NULL, weekday int NOT NULL);
+# """)
+
+# INSERT RECORDS
+
+# songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, \
+# artist_id, session_id, location, user_agent) \
+# VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+# """)
+
+user_table_insert = ("""INSERT INTO users (user_id, contact_email, phone, browser_ip, order_number) \
+VALUES (%s, %s, %s, %s, %s) \
+ON CONFLICT (user_id)\
+DO UPDATE SET level = EXCLUDED.level;""")
+
+orders_table_insert = ("""INSERT INTO songs (id, total_price, name, order_number, order_status_url) \
+VALUES (%s, %s, %s, %s, %s) \
+ON CONFLICT (id)\
+DO NOTHING;""")
+
+# artist_table_insert = ("""INSERT INTO artists (artist_id, name, location, lattitude, longitude) \
+# VALUES (%s, %s, %s, %s, %s)
+# ON CONFLICT (artist_id)\
+# DO NOTHING;""")
+#
+# time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, \
+# year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) \
+# ON CONFLICT (start_time)\
+# DO NOTHING;""")
+
+# FIND SONGS
+
+orders_select = ("""SELECT id, orders.id FROM orders \
+JOIN users ON orders.order_number = users.order_number \
+WHERE orders.id = %s AND users.id = %s AND orders.total_price = %s\
+;""")
+
+# QUERY LISTS
+
+# create_table_queries = [songplay_table_create, user_table_create,
+#                         song_table_create, artist_table_create, time_table_create]
+# drop_table_queries = [songplay_table_drop, user_table_drop,
+#                       song_table_drop, artist_table_drop, time_table_drop]
+
+create_table_queries = [user_table_create, orders_table_create]
+drop_table_queries = [users_table_drop, orders_table_drop]
